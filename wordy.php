a698d44b7aed462cc7f179a01c917426db3b724a
ELmardi2 <mardi.yahia@gmail.com>
2016-07-12 14:53:38 +0200

    first

--- wordy.php
+++ wordy.php
@@ -4,10 +4,14 @@
 // This is only a SKELETON file for the "Hello World" exercise.
 // It's been provided as a convenience to get you started writing code faster.
 //
-
-function calculate($question = "")
-{
-    //
-    // YOUR CODE GOES HERE
-    //
+ function HammingDistance($x, $y) {
+    $a1 = str_split($x);
+    $a2 = str_split($y);
+    $dh = 0;
+    for ($i = 0; $i < count($a1); $i++)
+        if($a1[$i] != $a2[$i]) $dh++;
+    return $dh;
 }
+
+echo HammingDistance('GAGCCTACTAACGGGAT,CATCGTAATGACGGCCT');
+  ?>
